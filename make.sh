81f17cc3a43b8cc1f2697847e2d04dd0d109c7ed
Chris Jordan <jordanchriss@gmail.com>
2019-08-07 11:09:06 -0400

    added creation of required build and lib folders to make.sh

--- make.sh
+++ make.sh
@@ -7,6 +7,9 @@ CXXLIBS="-L./lib -L/usr/lib/x86_64-linux-gnu"
 COMMON_FLAGS="-fPIC -g -std=c++11"
 OPTIMIZATION_FLAGS="-DNDEBUG -O3"
 
+mkdir -p build
+mkdir -p lib
+
 echo "Compiling RTM"
 $GCC -c $CXXINCLUDES $CXXLIBS $COMMON_FLAGS $OPTIMIZATION_FLAGS src/MeshIO.cpp -o build/MeshIO.o
 $GCC -c $CXXINCLUDES $CXXLIBS $COMMON_FLAGS $OPTIMIZATION_FLAGS src/TaskMesher.cpp -o build/TaskMesher.o
